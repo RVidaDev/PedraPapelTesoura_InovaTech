55922d7aa5c06e2790e64e0de9ef61e7c93a9212
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:16:59 -0300

    def's

---
+++ ppp.py
@@ -0,0 +1,17 @@
+def obter_escolha_usuario():
+    int("Escolha um"))
+
+
+def gerar_escolha_computador():
+    pass
+
+
+def determinar_vencedor():
+    
+
+def jogar_novamente():
+    pass
+
+def jogar():
+    if(__name__="__main__"):
+        pass
