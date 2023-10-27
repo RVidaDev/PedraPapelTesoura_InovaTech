4c17257c1a01d42ff4eaa67ebade6b9e46257a15
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:11:47 -0300

    def obter escolha

---
+++ ppp.py
@@ -0,0 +1,17 @@
+def obter_escolha_usuario():
+    escolha = input("pedra, papel ou tesoura?: ").lower()
+
+    if escolha == "pedra":
+        print("Sua escolha foi pedra!")
+        return escolha
+    elif escolha == "papel":
+        print("sua escolha foi papel")
+        return escolha
+    elif escolha == "tesoura":
+        print("sua escolha foi tesoura")
+        return escolha
+
+def jogar():
+
+if(__name__=="__main__"):
+    jogar()
