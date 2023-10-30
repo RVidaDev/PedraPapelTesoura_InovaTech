fa7b6d98461adde6975011ac07ada2271f2beb7a
RVidaDev <vidraf9@gmail.com>
2023-10-30 17:07:49 -0300

    so falta a def jogar()

--- ppp.py
+++ ppp.py
@@ -12,8 +12,36 @@ def obter_escolha_usuario():
     elif escolha_usuario == "tesoura":
         print("sua escolha foi tesoura")
         return escolha_usuario
-    
 
+def gerar_escolha_computador():
+   
+    opcao =["papel", "pedra", "tesoura"]
+    escolha_computador = random.choice(opcao)
+    
+    print("o computador escolheu: ", escolha_computador)
+
+    return(escolha_computador)
+
+def determinar_vencedor(obter_escolha_usuario, gerar_escolha_computador):
+    
+    if (obter_escolha_usuario == "pedra" and gerar_escolha_computador == "tesoura") or \
+        (obter_escolha_usuario == "tesoura" and gerar_escolha_computador == "papel") or \
+        (obter_escolha_usuario == "papel" and gerar_escolha_computador == "pedra"):
+                return "você ganhou! {} é maior que {}" .format(obter_escolha_usuario, gerar_escolha_computador)
+    elif obter_escolha_usuario == gerar_escolha_computador:
+        return "empate! ambos escolheram {}" .format(obter_escolha_usuario)
+    else:
+        return "você perdeu. {} é maior que {}" .format(gerar_escolha_computador, obter_escolha_usuario)
+
+
+def jogar_novamente():
+    
+    jogarAgain = input("deseja jogar de novo?: ").lower
+    
+    while jogarAgain == "sim":
+        print("ok, vou rodar de novo")
+        return jogar_novamente
+        
     
 
 
